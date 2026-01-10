c87c3672e70084b12336ce0b59ad6081b53b0754
KrPrince19 <princekumarydv19@gmail.com>
2026-01-10 11:57:40 +0530

    Update page.js

--- app/Signup/page.js
+++ app/Signup/page.js
@@ -42,7 +42,7 @@ export default function SignupPage() {
     setLoading(true);
 
     try {
-      const res = await fetch("https://care-nest-backend.vercel.app/signup", {
+      const res = await fetch("https://carenestbackend-1.onrender.com/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
